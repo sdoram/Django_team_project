9f5dabd40b1a2e23f7065cb5af565434e4c3eab3
byungjin <byungjinku@naver.com>
2023-04-13 21:59:49 +0900

    게시글 작성 수정 삭제 구현
    데이터 베이스 연결 불가
    
    django.db.utils.IntegrityError: NOT NULL constraint failed: posting_posting.username_id

--- comment/admin.py
+++ comment/admin.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
+<<<<<<< Updated upstream
 # Register your models here.
+=======
+from .models import Posting
+
+# Register your models here.
+>>>>>>> Stashed changes
